c064cb79e0852dd6084a3024f1d47c8630bd8e79
Ben Bamber <benbamber@Bens-Air.lan>
2024-07-20 21:36:29 +0100

    revert form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "email@motatec.co.uk";
+    $email_to = "mr.ben.bamber@gmail.com";
     $email_subject = "New Contact Form Submission";
  
     function died($error) {
